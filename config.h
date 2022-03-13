e74c8235cf27de6747bbd9a26fed1d52ce901468
personinblack <berkay@tuta.io>
2022-03-13 18:41:47 +0300

    Change dunst_history keymap and organize things a little

--- config.h
+++ config.h
@@ -80,63 +80,63 @@ static const char *dmenucmd[] = {
 	"dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont,
 	"-nb", col_normbg, "-nf", col_normfg, "-sb", col_selfg, "-sf", col_black, NULL
 };
-static const char *termcmd[]  = { "st", NULL };
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle",  NULL };
-static const char *screenshot[] = { "/usr/bin/scrot", "-e", "mv $f ~/screenshots", NULL };
+static const char *termcmd[]           = { "st", NULL };
+static const char *upvol[]             = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
+static const char *downvol[]           = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
+static const char *mutevol[]           = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle",  NULL };
+static const char *screenshot[]        = { "/usr/bin/scrot", "-e", "mv $f ~/screenshots", NULL };
 static const char *screenshot_select[] = { "/usr/bin/scrot", "-e", "mv $f ~/screenshots", "--select", NULL };
-static const char *dunst_close[] = { "/usr/bin/dunstctl", "close" };
-static const char *dunst_close_all[] = { "/usr/bin/dunstctl", "close-all" };
-static const char *dunst_history[] = { "/usr/bin/dunstctl", "history-pop" };
+static const char *dunst_close[]       = { "/usr/bin/dunstctl", "close" };
+static const char *dunst_close_all[]   = { "/usr/bin/dunstctl", "close-all" };
+static const char *dunst_history[]     = { "/usr/bin/dunstctl", "history-pop" };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ 0,                            XK_Print,  spawn,          {.v = screenshot } },
-	{ ShiftMask,                    XK_Print,  spawn,          {.v = screenshot_select } },
-	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
-	{ 0,                            XF86XK_AudioMute,        spawn, {.v = mutevol } },
-	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
-	{ ControlMask|Mod1Mask,         XK_space,  spawn,          {.v = dunst_close } },
-	{ ControlMask|ShiftMask,        XK_space,  spawn,          {.v = dunst_close_all } },
-	{ ControlMask|ShiftMask,        XK_BackSpace, spawn,       {.v = dunst_history } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = +1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = 0  } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {0} },
+	/* modifier                     key                      function        argument */
+	{ MODKEY,                       XK_d,                    spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return,               spawn,          {.v = termcmd } },
+	{ 0,                            XK_Print,                spawn,          {.v = screenshot } },
+	{ ShiftMask,                    XK_Print,                spawn,          {.v = screenshot_select } },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute,        spawn,          {.v = mutevol } },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn,          {.v = upvol   } },
+	{ ControlMask|Mod1Mask,         XK_space,                spawn,          {.v = dunst_close } },
+	{ ControlMask|ShiftMask,        XK_space,                spawn,          {.v = dunst_close_all } },
+	{ ControlMask|Mod1Mask,         XK_BackSpace,            spawn,          {.v = dunst_history } },
+	{ MODKEY,                       XK_b,                    togglebar,      {0} },
+	{ MODKEY,                       XK_j,                    focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,                    focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,                    incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_p,                    incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,                    setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,                    setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return,               zoom,           {0} },
+	{ MODKEY,                       XK_Tab,                  view,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,                    killclient,     {0} },
+	{ MODKEY,                       XK_t,                    setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,                    setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,                    setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_f,                    togglefullscr,  {0} },
+	{ MODKEY,                       XK_space,                setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,                togglefloating, {0} },
+	{ MODKEY,                       XK_0,                    view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                    tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,                focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,               focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,                tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,               tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,                setgaps,        {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_minus,                setgaps,        {.i = +1 } },
+	{ MODKEY,                       XK_equal,                setgaps,        {.i = 0  } },
+	TAGKEYS(                        XK_1,                                    0)
+	TAGKEYS(                        XK_2,                                    1)
+	TAGKEYS(                        XK_3,                                    2)
+	TAGKEYS(                        XK_4,                                    3)
+	TAGKEYS(                        XK_5,                                    4)
+	TAGKEYS(                        XK_6,                                    5)
+	TAGKEYS(                        XK_7,                                    6)
+	TAGKEYS(                        XK_8,                                    7)
+	TAGKEYS(                        XK_9,                                    8)
+	{ MODKEY|ControlMask|ShiftMask, XK_q,                    quit,           {0} },
 };
 
 /* button definitions */
