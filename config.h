dd7d254ec25715061bc1f9f9f98fed0cd84d2904
personinblack <berkay@tuta.io>
2022-12-03 17:24:37 +0300

    Make gaps smaller

--- config.h
+++ config.h
@@ -12,7 +12,7 @@
 
 /* appearance */
 static const unsigned int borderpx      = 0;        /* border pixel of windows */
-static const unsigned int gappx         = 40;       /* gaps between windows */
+static const unsigned int gappx         = 15;       /* gaps between windows */
 static const unsigned int snap          = 1;        /* snap pixel */
 static const int showbar                = 1;        /* 0 means no bar */
 static const int topbar                 = 1;        /* 0 means bottom bar */
