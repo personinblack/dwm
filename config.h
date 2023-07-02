80aeca493192a968bdd13b9e3d4ae393df218546
personinblack <berkay@tuta.io>
2023-07-02 22:07:31 +0300

    Add "ewmhtags" patch in the expense of gaps

--- config.h
+++ config.h
@@ -12,6 +12,7 @@
 
 /* appearance */
 static const unsigned int borderpx      = 0;        /* border pixel of windows */
+static const int startwithgaps          = 1;        /* 1 means gaps are used by default */
 static const unsigned int gappx         = 7;        /* gaps between windows */
 static const unsigned int snap          = 1;        /* snap pixel */
 static const int showbar                = 1;        /* 0 means no bar */
@@ -133,9 +134,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period,               focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,                tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,               tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,                setgaps,        {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_minus,                setgaps,        {.i = +1 } },
-	{ MODKEY,                       XK_equal,                setgaps,        {.i = 0  } },
+	// { MODKEY,                       XK_minus,                setgaps,        {.i = -1 } },
+	// { MODKEY|ShiftMask,             XK_minus,                setgaps,        {.i = +1 } },
+	// { MODKEY|ShiftMask,             XK_asterisk,             setgaps,        {.i = GAP_RESET  } },
+	// { MODKEY,                       XK_asterisk,             setgaps,        {.i = GAP_TOGGLE  } },
 	TAGKEYS(                        XK_1,                                    0)
 	TAGKEYS(                        XK_2,                                    1)
 	TAGKEYS(                        XK_3,                                    2)
